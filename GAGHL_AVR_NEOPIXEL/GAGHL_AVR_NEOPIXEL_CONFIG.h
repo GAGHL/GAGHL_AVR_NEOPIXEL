106498af5c834451bfd57f30b758d43be6aea279
GAGHL <123871450+GAGHL@users.noreply.github.com>
2025-08-18 12:55:23 +0330

    Add files via upload
    In the name of GOD
    Adding GAGHL_AVR_NEOPIXEL_CONFIG.h file

---
+++ GAGHL_AVR_NEOPIXEL/GAGHL_AVR_NEOPIXEL_CONFIG.h
@@ -0,0 +1,30 @@
+/*
+ * CONFIG.h
+ *
+ * Created: 8/18/2025 12:48:57 PM
+ *  Author: GAGHL
+ */ 
+
+
+#ifndef GAGHL_AVR_NEOPIXEL_CONFIG_H_
+#define GAGHL_AVR_NEOPIXEL_CONFIG_H_
+
+#define F_CPU 8000000UL
+
+#if defined(__AVR_ATtiny13__) || defined(__AVR_ATtiny13A__)
+
+#ifndef F_CPU
+#define F_CPU 9600000UL
+#warning "F_CPU is not defined! Default value will be used."
+#endif
+
+#else // If MCU is not ATtiny13 or ATtiny13A
+
+#ifndef F_CPU
+#define F_CPU 8000000UL
+#warning "F_CPU is not defined! Default value will be used."
+#endif
+
+#endif
+
+#endif /* GAGHL_AVR_NEOPIXEL_CONFIG_H_ */
